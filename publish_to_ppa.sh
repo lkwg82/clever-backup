ee98f2a6459dd440f2024fce59190efce3c68cec
Lars K.W. Gohlke <lkwg82@gmx.de>
2014-12-29 02:10:57 +0100

    pimped publish script

--- publish_to_ppa.sh
+++ publish_to_ppa.sh
@@ -2,6 +2,9 @@
 
 set -e 
 
+ppa="ppa:lkwg82/clever-backup"
+
+
 git-dch --release --git-author --commit --id-length=10 \
 	&& git-buildpackage -S -sa --git-tag --git-sign-tags --git-no-create-orig \
-	&& echo dput ppa:lkwg82/clever-backup ../clever-backup-1_1-2ubuntu1_source.changes
+	&& echo dput $ppa $(find ../clever-backup*source.changes | sort | tail -n1)

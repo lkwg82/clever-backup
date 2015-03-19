f87d94250f4a8f6152d7ba2b00dc24d05dfd3c13
Lars K.W. Gohlke <lkwg82@gmx.de>
2015-03-19 20:00:18 +0100

    added changelog-script

--- publish_to_ppa.sh
+++ publish_to_ppa.sh
@@ -8,4 +8,5 @@ ppa="ppa:lkwg82/clever-backup"
 git-dch --release --git-author --commit --id-length=10 \
 	&& git-buildpackage -S -sa --git-tag --git-sign-tags --git-no-create-orig \
 	&& dput $ppa $(find ../clever-backup*source.changes | sort | tail -n1)
+	&& ./changelog.sh
 	&& git push origin --tags

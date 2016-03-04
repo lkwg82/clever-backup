f419c75bb19532193aa468c6617cfd019f472d2b
Lars K.W. Gohlke <lkwg82@gmx.de>
2016-03-05 00:10:53 +0100

    added git push

--- publish_to_ppa.sh
+++ publish_to_ppa.sh
@@ -9,4 +9,5 @@ gbp dch --release --git-author --commit --id-length=10 \
 	&& gbp buildpackage -S -sa --git-tag --git-sign-tags --git-no-create-orig \
 	&& dput $ppa $(find ../clever-backup*source.changes | sort | tail -n1) \
 	&& ./changelog.sh \
+    && git push \
 	&& git push origin --tags

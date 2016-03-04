8d36f58a92b3c2653d24973e24de24e104fa2c07
Lars K.W. Gohlke <lkwg82@gmx.de>
2016-03-05 00:09:57 +0100

    fixed script error

--- publish_to_ppa.sh
+++ publish_to_ppa.sh
@@ -7,6 +7,6 @@ ppa="ppa:lkwg82/clever-backup"
 
 gbp dch --release --git-author --commit --id-length=10 \
 	&& gbp buildpackage -S -sa --git-tag --git-sign-tags --git-no-create-orig \
-	&& dput $ppa $(find ../clever-backup*source.changes | sort | tail -n1)
-	&& ./changelog.sh
+	&& dput $ppa $(find ../clever-backup*source.changes | sort | tail -n1) \
+	&& ./changelog.sh \
 	&& git push origin --tags
